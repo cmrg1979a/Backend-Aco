b33cfbe44c93e54f95fbb5d96284b74ffb820ad8
Jeanpierre Zurita <ophetel@gmail.com>
2023-06-20 00:44:20 -0500

    proyeccion 50%

--- src/controllers/proyeccioncontroller.ts
+++ src/controllers/proyeccioncontroller.ts
@@ -30,7 +30,7 @@ export const setProyeccion = async (req: Request, res: Response) => {
         return item.id_coin;
       }),
       data.details.map((item) => {
-        return item.descripcion;
+        return item.description;
       }),
       data.details.map((item) => {
         return item.monto_monlocal;
@@ -69,16 +69,243 @@ export const listProyeccion = async (req: Request, res: Response) => {
   await pool.query(
     "SELECT * FROM function_list_proyeccion($1,$2,$3,$4,$5,$6,$7,$8,$9);",
     [
-      req.query.pid_branch ? req.query.pid_branch : null,
-      req.query.pid_month ? req.query.pid_month : null,
-      req.query.pid_year ? req.query.pid_year : null,
-      req.query.pid_user ? req.query.pid_user : null,
-      req.query.ptipocambio ? req.query.ptipocambio : null,
-      req.query.ptotal_monlocal ? req.query.ptotal_monlocal : null,
-      req.query.ptotal_conversionext ? req.query.ptotal_conversionext : null,
-      req.query.ptotal_monext ? req.query.ptotal_monext : null,
-      req.query.ptotal_proyectado_ext ? req.query.ptotal_proyectado_ext : null,
+      req.query.id_branch ? req.query.id_branch : null,
+      req.query.id_month ? req.query.id_month : null,
+      req.query.id_year ? req.query.id_year : null,
+      req.query.id_user ? req.query.id_user : null,
+      req.query.tipocambio ? req.query.tipocambio : null,
+      req.query.total_monlocal ? req.query.total_monlocal : null,
+      req.query.total_conversionext ? req.query.total_conversionext : null,
+      req.query.total_monext ? req.query.total_monext : null,
+      req.query.total_proyectado_ext ? req.query.total_proyectado_ext : null,
     ],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const verProyeccion = async (req: Request, res: Response) => {
+  await pool.query(
+    "SELECT * FROM function_proyeccion_ver($1);",
+    [req.query.id ? req.query.id : null],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+
+export const updateProyeccion = async (req: Request, res: Response) => {
+  let data = req.body;
+  console.log(data);
+  await pool.query(
+    "SELECT * FROM function_proyeccion_editar($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18);",
+    [
+      data.id,
+      data.id_branch,
+      data.id_month,
+      data.id_year,
+      data.id_user,
+      data.tipocambio,
+      data.total_monlocal,
+      data.total_conversionext,
+      data.total_monext,
+      data.total_proyectado_ext,
+      data.details.map((item) => {
+        return item.id ? item.id : null;
+      }),
+      data.details.map((item) => {
+        return item.id_tiposubgasto;
+      }),
+      data.details.map((item) => {
+        return item.id_entitie;
+      }),
+      data.details.map((item) => {
+        return item.id_coin;
+      }),
+      data.details.map((item) => {
+        return item.description ? item.description : null;
+      }),
+      data.details.map((item) => {
+        return item.monto_monlocal;
+      }),
+      data.details.map((item) => {
+        return item.monto_monext;
+      }),
+      data.details.map((item) => {
+        return item.estado;
+      }),
+    ],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+
+export const validateProyeccionAprob = async (req: Request, res: Response) => {
+  await pool.query(
+    "SELECT * FROM function_proyeccion_validar_aprob($1,$2);",
+    [req.query.id_month, req.query.id_year],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const copiarProyeccion = async (req: Request, res: Response) => {
+  await pool.query(
+    "SELECT * FROM function_proyeccion_copiar($1,$2,$3,$4,$5);",
+    [
+      req.body.id,
+      req.body.id_month,
+      req.body.id_year,
+      req.body.id_user,
+      req.body.id_branch,
+    ],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const aprobarProyeccion = async (req: Request, res: Response) => {
+  console.log(req.body.id);
+  await pool.query(
+    "SELECT * FROM function_proyeccion_aprobar($1);",
+    [req.body.id],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        if (!!rows[0].estadoflag) {
+          res.json({
+            status: 200,
+            statusBol: true,
+            data: rows,
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        } else {
+          res.json({
+            status: 200,
+            statusBol: true,
+
+            mensaje: rows[0].mensaje,
+            estado: rows[0].estadoflag,
+          });
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const eliminarProyeccion = async (req: Request, res: Response) => {
+  console.log(req.body.id);
+  await pool.query(
+    "SELECT * FROM function_proyeccion_eliminar($1);",
+    [req.body.id],
     (err, response, fields) => {
       if (!err) {
         let rows = response.rows;
--- src/routes/proyeccion.ts
+++ src/routes/proyeccion.ts
@@ -1,8 +1,27 @@
 import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
-import { listProyeccion, setProyeccion } from "../controllers/proyeccioncontroller";
+import {
+  aprobarProyeccion,
+  copiarProyeccion,
+  eliminarProyeccion,
+  listProyeccion,
+  setProyeccion,
+  updateProyeccion,
+  validateProyeccionAprob,
+  verProyeccion,
+} from "../controllers/proyeccioncontroller";
 router.post("/insertar_proyeccion", TokenValidation, setProyeccion);
 router.get("/list_proyeccion", TokenValidation, listProyeccion);
+router.get("/ver_proyeccion", TokenValidation, verProyeccion);
+router.put("/update_proyeccion", TokenValidation, updateProyeccion);
+router.put(
+  "/validate_proyeccion_aprobacion",
+  TokenValidation,
+  validateProyeccionAprob
+);
+router.put("/copiar_proyeccion", TokenValidation, copiarProyeccion);
+router.put("/aprobar_proyeccion", TokenValidation, aprobarProyeccion);
+router.put("/eliminar_proyeccion", TokenValidation, eliminarProyeccion);
 
 export default router;
