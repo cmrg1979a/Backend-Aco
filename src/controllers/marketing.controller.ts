06bf495edf4cd8d0dbf570f1057c28fb2a3d3c5b
ffarro <farrofiestas@gmail.com>
2023-09-22 01:50:25 -0500

    Add RF2023_004 - tipo de marketing

--- src/controllers/marketing.controller.ts
+++ src/controllers/marketing.controller.ts
@@ -7,7 +7,6 @@ const { Pool } = pg;
 const pool = conexion();
 
 export const getListMarketing = async (req: Request, res: Response) => {
-  console.log("Listando desde el back");
   let data = req.query;
   const result = await pool.query("SELECT *from function_list_table_marketing($1,$2, $3, $4, $5);", [
     data.id_branch,
@@ -21,14 +20,12 @@ export const getListMarketing = async (req: Request, res: Response) => {
 
   try {
       if (rows.length > 0) {
-          console.log(rows);
-          return res.status(200).json(rows);
+        return res.status(200).json(rows);
       } else {
-          return res.status(404).json({ error: 'No se encontraron resultados.' });
+        console.log("No se encontraron resultados");
       }
   } catch (error) {
-      console.error('Error al listar los resultados:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al listar los resultados:', error);
   }
 }
 
@@ -46,14 +43,12 @@ export const insertMarketing = async (req: Request, res: Response) => {
 
   try {
       if (rows.length > 0) {
-        const { status, message } = rows[0];
-        return res.status(200).json({ status, message });
+        return res.status(200).json(rows);
       } else {
-          return res.status(500).json({ error: 'No se pudo insertar el registro.' });
+        console.log('No se pudo insertar el registro.');
       }
   } catch (error) {
-      console.error('Error al insertar el registro:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al insertar el registro:', error);
   }
 }
 
@@ -66,25 +61,24 @@ export const readMarketing = async (req: Request, res: Response) => {
   const { rows } = result;
 
   try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-          return res.status(500).json({ error: 'No se pudo accede al registro.' });
-      }
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
   } catch (error) {
-      console.error('Error al acceder al registro:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al accceder el registro:', error);
   }
 }
 
 export const updateMarketing = async (req: Request, res: Response) => {
   let data = req.body;
-
-  const result = await pool.query("SELECT *from function_edit_table_marketing($1,$2, $3, $4, $5);", [
+  const result = await pool.query("SELECT *from function_edit_table_marketing($1,$2, $3, $4, $5, $6);", [
     data.id,
     data.name,
     data.description,
     data.position,
+    data.id_branch,
     data.status
   ]);
 
@@ -92,14 +86,12 @@ export const updateMarketing = async (req: Request, res: Response) => {
 
   try {
       if (rows.length > 0) {
-        const { status, message } = rows[0];
-        return res.status(200).json({ status, message });
+        return res.status(200).json(rows);
       } else {
-          return res.status(500).json({ error: 'No se pudo actualizar el registro.' });
+        console.log("No se pudo actualizar el registro");
       }
   } catch (error) {
-      console.error('Error al actualizar el registro:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al actualizar el registro:', error);
   }
 }
 
@@ -113,35 +105,51 @@ export const deleteMarketing = async (req: Request, res: Response) => {
 
   try {
       if (rows.length > 0) {
-        const { status, message } = rows[0];
-        return res.status(200).json({ status, message });
+        return res.status(200).json(rows);
       } else {
-          return res.status(500).json({ error: 'No se pudo eliminar el registro.' });
+          console.log('No se pudo eliminar el registro.');
       }
   } catch (error) {
-      console.error('Error al eliminar el registro:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al eliminar el registro:', error);
   }
 }
 
 export const validatePositionMarketing = async (req: Request, res: Response) => {
-  const result = await pool.query("SELECT *from function_validates_type_document_and_document_in_enterprise($1,$2);", [
-    req.query.id_document,
-    req.query.document,
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_validated_position_table_marketing($1,$2, $3);", [
+    data.id ? data.id : 0,
+    data.id_branch,
+    data.position ? data.position : null
   ]);
 
   const { rows } = result;
 
   try {
-      if (rows.length > 0) {
-        console.log(rows);
-          const { status, message } = rows[0];
-          return res.status(200).json({ status, message });
-      } else {
-          return res.status(404).json({ error: 'No se respuesta.' });
-      }
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log("No se encontraron resultados");
+    }
   } catch (error) {
-      console.error('Error al listar las regiones:', error);
-      return res.status(500).json({ error: 'Error interno del servidor.' });    
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const lastPositionMarketing = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_next_position_table_marketing($1);", [
+    data.id_branch
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log("No se encontraron resultados");
+    }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
   }
 }
--- src/routes/marketing.ts
+++ src/routes/marketing.ts
@@ -9,14 +9,16 @@ import {
   updateMarketing,
   deleteMarketing,
   validatePositionMarketing,
+  lastPositionMarketing,
 } from "../controllers/marketing.controller";
 
 router.get("/getListMarketing/", getListMarketing);
-router.post("/insertMarketing/", TokenValidation, insertMarketing);
-router.get("/readMarketing/", TokenValidation, readMarketing);
-router.put("/deleteMarketing/", TokenValidation, deleteMarketing);
-router.put("/updateMarketing/", TokenValidation ,updateMarketing);
-router.get("/validatePositionMarketing/", TokenValidation, validatePositionMarketing);
+router.post("/insertMarketing/", insertMarketing);
+router.get("/readMarketing/", readMarketing);
+router.put("/deleteMarketing/", deleteMarketing);
+router.put("/updateMarketing/", updateMarketing);
+router.get("/validatePositionMarketing/",  validatePositionMarketing);
+router.get("/lastPositionMarketing/",  lastPositionMarketing);
 
 
 export default router;
