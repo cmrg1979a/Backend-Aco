174d057994e4b2653fbe163020cfe5a144786526
ffarro <farrofiestas@gmail.com>
2023-09-27 18:59:28 -0500

    add RF2023_029 - table_year

--- src/controllers/yearController.ts
+++ src/controllers/yearController.ts
@@ -30,3 +30,105 @@ export const getChargeYear = async (req: Request, res: Response) => {
     }
   );
 };
+
+export const getListYear = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_list_table_year($1,$2, $3);", [
+    data.id_branch,
+    data.code ? data.code : null,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const insertYear = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_insert_table_year($1,$2, $3);", [
+    data.code,
+    data.status,
+    data.id_branch,
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log('No se pudo insertar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al insertar el registro:', error);
+  }
+}
+
+export const readYear = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_see_table_year($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
+  } catch (error) {
+      console.log('Error al accceder el registro:', error);
+  }
+}
+
+export const updateYear = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_edit_table_year($1,$2, $3, $4);", [
+    data.id,
+    data.code,
+    data.status,
+    data.id_branch
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se pudo actualizar el registro");
+      }
+  } catch (error) {
+      console.log('Error al actualizar el registro:', error);
+  }
+}
+
+export const deleteYear = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_delete_table_year($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+          console.log('No se pudo eliminar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al eliminar el registro:', error);
+  }
+}
--- src/routes/year.ts
+++ src/routes/year.ts
@@ -2,9 +2,22 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getChargeYear} from "../controllers/yearController";
+import { 
+    getChargeYear,
+    getListYear,
+    insertYear,
+    readYear,
+    updateYear,
+    deleteYear,
+} from "../controllers/yearController";
 
 router.get("/getChargeYear", TokenValidation, getChargeYear);
 // router.post("/getPortEnd", TokenValidation, getPortEnd);
+
+router.get("/getListYear/", getListYear);
+router.post("/insertYear/", insertYear);
+router.get("/readYear/", readYear);
+router.put("/updateYear/", updateYear);
+router.put("/deleteYear/", deleteYear);
 
 export default router;
