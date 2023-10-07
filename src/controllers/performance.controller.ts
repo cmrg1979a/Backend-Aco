15186f988eda24b5f01a2aaf70dcc6c11ae52ebc
ffarro <farrofiestas@gmail.com>
2023-10-07 05:15:35 -0500

    add tb performance

--- src/controllers/performance.controller.ts
+++ src/controllers/performance.controller.ts
@@ -33,3 +33,107 @@ export const getPerformances = async (req: Request, res: Response) => {
     }
   );
 };
+
+export const getListPerformance = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_list_performance($1,$2, $3, $4);", [
+    data.id_branch,
+    data.code ? data.code : null,
+    data.description ? data.description : null,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const insertPerformance = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_insert_performance($1,$2, $3);", [
+    data.description,
+    data.status,
+    data.id_branch
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log('No se pudo insertar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al insertar el registro:', error);
+  }
+}
+
+export const readPerformance = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_see_table_performance($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
+  } catch (error) {
+      console.log('Error al accceder el registro:', error);
+  }
+}
+
+export const updatePerformance = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_edit_performance($1,$2, $3);", [
+    data.id,
+    data.description,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se pudo actualizar el registro");
+      }
+  } catch (error) {
+      console.log('Error al actualizar el registro:', error);
+  }
+}
+
+export const switchPerformance = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_switch_performance($1, $2);", [
+    data.id,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+          console.log('No se pudo eliminar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al eliminar el registro:', error);
+  }
+}
+
--- src/routes/perfomance.ts
+++ src/routes/perfomance.ts
@@ -2,8 +2,20 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getPerformances } from "../controllers/performance.controller";
+import { 
+    getPerformances ,
+    insertPerformance,
+    getListPerformance,
+    switchPerformance,
+    updatePerformance, 
+    readPerformance
+} from "../controllers/performance.controller";
 
 router.post("/getPerfomance", TokenValidation, getPerformances);
+router.get("/getListPerformance/", TokenValidation, getListPerformance);
+router.post("/insertPerformance/", TokenValidation, insertPerformance);
+router.get("/readPerformance/", TokenValidation, readPerformance);
+router.put("/updatePerformance/", TokenValidation, updatePerformance);
+router.put("/switchPerformance/", TokenValidation, switchPerformance);
 
 export default router;
