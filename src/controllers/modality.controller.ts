e02218d5c185fb9c3e2c0408f4a6eeca1bc18df1
ffarro <farrofiestas@gmail.com>
2023-10-17 17:27:16 -0500

    add table modality

--- src/controllers/modality.controller.ts
+++ src/controllers/modality.controller.ts
@@ -30,3 +30,112 @@ export const getModality = async (req: Request, res: Response) => {
     }
   );
 };
+
+
+export const getListModality = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_list_table_modality($1,$2, $3, $4, $5);", [
+    data.id_branch,
+    data.code ? data.code : null,
+    data.name ? data.name : null,
+    data.description ? data.description : null,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const insertModality = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_insert_table_modality($1,$2, $3, $4, $5);", [
+    data.id_branch,
+    data.name,
+    data.code,
+    data.description,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log('No se pudo insertar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al insertar el registro:', error);
+  }
+}
+
+export const readModality = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_see_table_modality($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
+  } catch (error) {
+      console.log('Error al accceder el registro:', error);
+  }
+}
+
+export const updateModality = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_edit_table_modality($1,$2, $3, $4);", [
+    data.id,
+    data.name,
+    data.description,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se pudo actualizar el registro");
+      }
+  } catch (error) {
+      console.log('Error al actualizar el registro:', error);
+  }
+}
+
+export const switchModality = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_switch_table_modality($1, $2);", [
+    data.id,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+          console.log('No se pudo eliminar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al eliminar el registro:', error);
+  }
+}
+
--- src/routes/modality.ts
+++ src/routes/modality.ts
@@ -2,8 +2,20 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getModality } from "../controllers/modality.controller";
+import { 
+    getModality,
+    getListModality,
+    insertModality,
+    readModality,
+    updateModality,
+    switchModality,
+} from "../controllers/modality.controller";
 
 router.post("/getModality", TokenValidation, getModality);
+router.get("/getListModality/", TokenValidation, getListModality);
+router.post("/insertModality/", TokenValidation, insertModality);
+router.get("/readModality/", TokenValidation, readModality);
+router.put("/updateModality/", TokenValidation, updateModality);
+router.put("/switchModality/", TokenValidation, switchModality);
 
 export default router;
