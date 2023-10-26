670a313fed69fc6d9525c2a10348d1401666bca8
ffarro <farrofiestas@gmail.com>
2023-10-25 19:06:47 -0500

    add mantenimiento de tipo de  carga

--- src/controllers/shipment.controller.ts
+++ src/controllers/shipment.controller.ts
@@ -32,3 +32,132 @@ export const getShipment = async (req: Request, res: Response) => {
     }
   );
 };
+
+export const getListShipment = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_list_table_shipment($1,$2, $3, $4, $5);", [
+    data.id_branch,
+    data.id_transport ? data.id_transport : null,
+    data.name ? data.name : null,
+    data.description ? data.description : null,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else { 
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const insertShipment = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_insert_table_shipment($1,$2, $3, $4, $5, $6);", [
+    data.id_branch,
+    data.code,
+    data.name,
+    data.id_transport,
+    data.description,
+    data.status,
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log('No se pudo insertar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al insertar el registro:', error);
+  }
+}
+
+export const readShipment = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_see_table_shipment($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
+  } catch (error) {
+      console.log('Error al accceder el registro:', error);
+  }
+}
+
+export const updateShipment = async (req: Request, res: Response) => {
+  let data = req.body;
+  
+  const result = await pool.query("SELECT *from function_edit_table_shipment($1,$2,$3,$4,$5);", [
+    data.id,
+    data.name,
+    data.description,
+    data.id_transport,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se pudo actualizar el registro");
+      }
+  } catch (error) {
+      console.log('Error al actualizar el registro:', error);
+  }
+}
+
+export const switchShipment = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_switch_table_shipment($1, $2);", [
+    data.id,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+          console.log('No se pudo eliminar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al eliminar el registro:', error);
+  }
+}
+
+export const getCargarTransport = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_cargar_table_transport($1);", [
+    data.id_branch
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else { 
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
--- src/routes/shipment.ts
+++ src/routes/shipment.ts
@@ -2,8 +2,23 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getShipment } from "../controllers/shipment.controller";
+import { 
+    getShipment,
+    getListShipment,
+    insertShipment,
+    readShipment,
+    updateShipment,
+    switchShipment,
+    getCargarTransport,
+} from "../controllers/shipment.controller";
 
 router.post("/getShipment", TokenValidation, getShipment);
+
+router.get("/getListShipment/", TokenValidation, getListShipment);
+router.post("/insertShipment/", TokenValidation, insertShipment);
+router.get("/readShipment/", TokenValidation, readShipment);
+router.put("/updateShipment/", TokenValidation, updateShipment);
+router.put("/switchShipment/", TokenValidation, switchShipment);
+router.get("/getCargarTransport/", TokenValidation, getCargarTransport);
 
 export default router;
