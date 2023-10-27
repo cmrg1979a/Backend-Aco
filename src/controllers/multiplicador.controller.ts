f92efe79ae81706800d9e258f5d4eba57db0aa5d
ffarro <farrofiestas@gmail.com>
2023-10-26 20:04:57 -0500

    add actions de multiplicador

--- src/controllers/multiplicador.controller.ts
+++ src/controllers/multiplicador.controller.ts
@@ -61,3 +61,110 @@ export const getMultiplicador = async (req: Request, res: Response) => {
     }
   );
 };
+
+export const getListMultiplicador = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_list_table_multiplicador($1,$2, $3, $4, $5, $6);", [
+    data.id_branch,
+    data.code ? data.code : null,
+    data.name ? data.name : null,
+    data.description ? data.description : null,
+    data.id_shipment ? data.id_shipment : null,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else { 
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
+
+export const readMultiplicador = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_see_table_multiplicador($1);", [
+    data.id
+  ]);
+
+  const { rows } = result;
+
+  try {
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      console.log('No se encontraron resultados.');
+    }
+  } catch (error) {
+      console.log('Error al accceder el registro:', error);
+  }
+}
+
+export const updateMultiplicador = async (req: Request, res: Response) => {
+  let data = req.body;
+  
+  const result = await pool.query("SELECT *from function_edit_table_multiplicador($1,$2,$3,$4,$5, $6);", [
+    data.id,
+    data.name,
+    data.description,
+    data.id_shipment,
+    data.valor,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+        console.log("No se pudo actualizar el registro");
+      }
+  } catch (error) {
+      console.log('Error al actualizar el registro:', error);
+  }
+}
+
+export const switchMultiplicador = async (req: Request, res: Response) => {
+  let data = req.body;
+  const result = await pool.query("SELECT *from function_switch_table_multiplicador($1, $2);", [
+    data.id,
+    data.status
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else {
+          console.log('No se pudo eliminar el registro.');
+      }
+  } catch (error) {
+      console.log('Error al eliminar el registro:', error);
+  }
+}
+
+export const getCargarShipment = async (req: Request, res: Response) => {
+  let data = req.query;
+  const result = await pool.query("SELECT *from function_cargar_table_shipment($1);", [
+    data.id_branch
+  ]);
+
+  const { rows } = result;
+
+  try {
+      if (rows.length > 0) {
+        return res.status(200).json(rows);
+      } else { 
+        console.log("No se encontraron resultados");
+      }
+  } catch (error) {
+      console.log('Error al listar los resultados:', error);
+  }
+}
--- src/routes/multiplicador.ts
+++ src/routes/multiplicador.ts
@@ -2,8 +2,21 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getMultiplicador } from "../controllers/multiplicador.controller";
+import { 
+    getMultiplicador,
+    getListMultiplicador,
+    readMultiplicador,
+    updateMultiplicador,
+    switchMultiplicador, 
+    getCargarShipment,
+} from "../controllers/multiplicador.controller";
 
 router.post("/getMultiplicador", TokenValidation, getMultiplicador);
+
+router.get("/getListMultiplicador/", TokenValidation, getListMultiplicador);
+router.get("/readMultiplicador/", TokenValidation, readMultiplicador);
+router.put("/updateMultiplicador/", TokenValidation, updateMultiplicador);
+router.put("/switchMultiplicador/", TokenValidation, switchMultiplicador);
+router.get("/getCargarShipment/", TokenValidation, getCargarShipment);
 
 export default router;
