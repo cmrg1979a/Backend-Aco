d1d32d0d09c27262c7573bb417603c06d819ff5b
ffarro <farrofiestas@gmail.com>
2023-10-29 23:00:47 -0500

    add standar a controller|rutas de table_marketing

--- src/controllers/marketing.controller.ts
+++ src/controllers/marketing.controller.ts
@@ -8,148 +8,136 @@ const pool = conexion();
 
 export const getListMarketing = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_list_table_marketing($1,$2, $3, $4, $5);", [
+  const result = await pool.query("SELECT *from function_listar_marketing($1,$2, $3, $4, $5);", [
     data.id_branch,
     data.name ? data.name : null,
     data.description ? data.description : null,
     data.position ? data.position : null,
     data.status
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-        console.log("No se encontraron resultados");
-      }
-  } catch (error) {
-      console.log('Error al listar los resultados:', error);
-  }
+  ], 
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
+    } else {
+      console.log(err);
+    }
+  });
 }
 
 export const insertMarketing = async (req: Request, res: Response) => {
   let data = req.body;
-  const result = await pool.query("SELECT *from function_insert_table_marketing($1,$2, $3, $4, $5);", [
+  await pool.query("SELECT *from function_insertar_marketing($1,$2, $3, $4, $5);", [
     data.name,
     data.description,
     data.position,
     data.status,
     data.id_branch,
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-        console.log('No se pudo insertar el registro.');
-      }
-  } catch (error) {
-      console.log('Error al insertar el registro:', error);
-  }
+  ],
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
+    } else {
+      console.log(err);
+    }
+  });
 }
 
 export const readMarketing = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_see_table_markeing($1);", [
+  await pool.query("SELECT *from function_ver_marketing($1);", [
     data.id
-  ]);
-
-  const { rows } = result;
-
-  try {
-    if (rows.length > 0) {
-      return res.status(200).json(rows);
+  ], 
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        data: rows,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+      });
     } else {
-      console.log('No se encontraron resultados.');
+      console.log(err);
     }
-  } catch (error) {
-      console.log('Error al accceder el registro:', error);
-  }
+  });
+
 }
 
 export const updateMarketing = async (req: Request, res: Response) => {
   let data = req.body;
-  const result = await pool.query("SELECT *from function_edit_table_marketing($1,$2, $3, $4, $5);", [
+  await pool.query("SELECT *from function_actualizar_marketing($1,$2, $3, $4, $5);", [
     data.id,
     data.name,
     data.description,
     data.position,
     data.status
-  ]);
+  ], (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        data: rows,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+      });
+    } else {
+      console.log(err);
+    }
+  });
 
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-        console.log("No se pudo actualizar el registro");
-      }
-  } catch (error) {
-      console.log('Error al actualizar el registro:', error);
-  }
 }
 
-export const switchMarketing = async (req: Request, res: Response) => {
-  let data = req.body;
-  const result = await pool.query("SELECT *from function_switch_table_marketing($1, $2);", [
-    data.id,
-    data.status
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-          console.log('No se pudo actualizar el registro.');
-      }
-  } catch (error) {
-      console.log('Error al actualizar el registro:', error);
-  }
-}
 
 export const validatePositionMarketing = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_validated_position_table_marketing($1,$2, $3);", [
+  await pool.query("SELECT *from function_validar_position_marketing($1,$2, $3);", [
     data.id ? data.id : 0,
     data.id_branch,
     data.position ? data.position : null
-  ]);
-
-  const { rows } = result;
-
-  try {
-    if (rows.length > 0) {
-      return res.status(200).json(rows);
+  ], (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        data: rows,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+      });
     } else {
-      console.log("No se encontraron resultados");
+      console.log(err);
     }
-  } catch (error) {
-      console.log('Error al listar los resultados:', error);
-  }
+  });
 }
 
-export const lastPositionMarketing = async (req: Request, res: Response) => {
+export const nextPositionMarketing = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_next_position_table_marketing($1);", [
+  await pool.query("SELECT *from function_buscar_siguiente_posicion_marketing($1);", [
     data.id_branch
-  ]);
-
-  const { rows } = result;
-
-  try {
-    if (rows.length > 0) {
-      return res.status(200).json(rows);
+  ], 
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        data: rows,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+      });
     } else {
-      console.log("No se encontraron resultados");
+      console.log(err);
     }
-  } catch (error) {
-      console.log('Error al listar los resultados:', error);
-  }
+  });
+
 }
