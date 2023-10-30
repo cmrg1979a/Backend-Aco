d276958046f87530c41a174802a94ae4fe0ec78f
ffarro <farrofiestas@gmail.com>
2023-10-30 02:09:52 -0500

    add standar a controller|rutas de table_performance

--- src/routes/perfomance.ts
+++ src/routes/perfomance.ts
@@ -6,16 +6,14 @@ import {
     getPerformances ,
     insertPerformance,
     getListPerformance,
-    switchPerformance,
     updatePerformance, 
     readPerformance
 } from "../controllers/performance.controller";
 
 router.post("/getPerfomance", TokenValidation, getPerformances);
-router.get("/getListPerformance/", TokenValidation, getListPerformance);
-router.post("/insertPerformance/", TokenValidation, insertPerformance);
-router.get("/readPerformance/", TokenValidation, readPerformance);
-router.put("/updatePerformance/", TokenValidation, updatePerformance);
-router.put("/switchPerformance/", TokenValidation, switchPerformance);
+router.get("/listar_performance/", TokenValidation, getListPerformance);
+router.post("/insertar_performance/", TokenValidation, insertPerformance);
+router.get("/ver_performance/", TokenValidation, readPerformance);
+router.put("/actualizar_performance/", TokenValidation, updatePerformance);
 
 export default router;
