968714d94c0a18bb12b230115f407e6673c02816
ffarro <farrofiestas@gmail.com>
2023-10-30 02:40:16 -0500

    add estandar a controller|routes en table_multiplicador

--- src/controllers/multiplicador.controller.ts
+++ src/controllers/multiplicador.controller.ts
@@ -64,107 +64,91 @@ export const getMultiplicador = async (req: Request, res: Response) => {
 
 export const getListMultiplicador = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_list_table_multiplicador($1,$2, $3, $4, $5, $6);", [
+  await pool.query("SELECT *from function_listar_multiplicador($1,$2, $3, $4, $5, $6);", [
     data.id_branch,
     data.code ? data.code : null,
     data.name ? data.name : null,
     data.description ? data.description : null,
     data.id_shipment ? data.id_shipment : null,
     data.status
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else { 
-        console.log("No se encontraron resultados");
-      }
-  } catch (error) {
-      console.log('Error al listar los resultados:', error);
-  }
+  ],
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
+    } else {
+      console.log(err);
+    }
+  });
 }
 
 export const readMultiplicador = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_see_table_multiplicador($1);", [
+  await pool.query("SELECT *from function_ver_multiplicador($1);", [
     data.id
-  ]);
-
-  const { rows } = result;
-
-  try {
-    if (rows.length > 0) {
-      return res.status(200).json(rows);
+  ],
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
     } else {
-      console.log('No se encontraron resultados.');
+      console.log(err);
     }
-  } catch (error) {
-      console.log('Error al accceder el registro:', error);
-  }
+  });
 }
 
 export const updateMultiplicador = async (req: Request, res: Response) => {
   let data = req.body;
   
-  const result = await pool.query("SELECT *from function_edit_table_multiplicador($1,$2,$3,$4,$5, $6);", [
+  await pool.query("SELECT *from function_actualizar_multiplicador($1,$2,$3,$4,$5, $6);", [
     data.id,
     data.name,
     data.description,
     data.id_shipment,
     data.valor,
     data.status
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-        console.log("No se pudo actualizar el registro");
-      }
-  } catch (error) {
-      console.log('Error al actualizar el registro:', error);
-  }
-}
-
-export const switchMultiplicador = async (req: Request, res: Response) => {
-  let data = req.body;
-  const result = await pool.query("SELECT *from function_switch_table_multiplicador($1, $2);", [
-    data.id,
-    data.status
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else {
-          console.log('No se pudo eliminar el registro.');
-      }
-  } catch (error) {
-      console.log('Error al eliminar el registro:', error);
-  }
+  ],
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
+    } else {
+      console.log(err);
+    }
+  });
 }
 
 export const getCargarShipment = async (req: Request, res: Response) => {
   let data = req.query;
-  const result = await pool.query("SELECT *from function_cargar_table_shipment($1);", [
+  await pool.query("SELECT *from function_cargar_shipment($1);", [
     data.id_branch
-  ]);
-
-  const { rows } = result;
-
-  try {
-      if (rows.length > 0) {
-        return res.status(200).json(rows);
-      } else { 
-        console.log("No se encontraron resultados");
-      }
-  } catch (error) {
-      console.log('Error al listar los resultados:', error);
-  }
+  ],
+  (err, response, fields) => {
+    if (!err) {
+      let rows = response.rows;
+      res.json({
+        status: 200,
+        estadoflag: rows[0].estadoflag,
+        mensaje: rows[0].mensaje,
+        data: rows
+      })
+    } else {
+      console.log(err);
+    }
+  });
 }
--- src/routes/multiplicador.ts
+++ src/routes/multiplicador.ts
@@ -7,16 +7,14 @@ import {
     getListMultiplicador,
     readMultiplicador,
     updateMultiplicador,
-    switchMultiplicador, 
     getCargarShipment,
 } from "../controllers/multiplicador.controller";
 
 router.post("/getMultiplicador", TokenValidation, getMultiplicador);
 
-router.get("/getListMultiplicador/", TokenValidation, getListMultiplicador);
-router.get("/readMultiplicador/", TokenValidation, readMultiplicador);
-router.put("/updateMultiplicador/", TokenValidation, updateMultiplicador);
-router.put("/switchMultiplicador/", TokenValidation, switchMultiplicador);
-router.get("/getCargarShipment/", TokenValidation, getCargarShipment);
+router.get("/listar_multiplicador/", TokenValidation, getListMultiplicador);
+router.get("/ver_multiplicador/", TokenValidation, readMultiplicador);
+router.put("/actualizar_multiplicador/", TokenValidation, updateMultiplicador);
+router.get("/cargar_shipment/", TokenValidation, getCargarShipment);
 
 export default router;
