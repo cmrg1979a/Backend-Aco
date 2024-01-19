92002683434fe66b9c7ccd812080e003aff4eeab
unknown <ophetel@gmail.com>
2024-01-19 08:40:01 -0500

    reversar

--- src/routes/databasePg.ts
+++ src/routes/databasePg.ts
@@ -8,30 +8,7 @@ let user = "";
 let password = "";
 let port = "";
 let database = "";
-if (process.env.NODE_ENV == "development") {
-  host = "67.205.129.62";
-  user = "chainsolver";
-  password = "Fr3sc0l1t4+";
-  port = "5432";
-  database = "chainsolver_db_v3";
-}
-
-if (process.env.NODE_ENV == "test") {
-  host = "67.205.129.62";
-  user = "chainsolver";
-  password = "Fr3sc0l1t4+";
-  port = "5432";
-  database = "chainsolver_db_v3";
-}
-if (process.env.NODE_ENV == "production") {
-  host = "10.116.0.2";
-  user = "postgres";
-  password = "@Developer2021Pic";
-  port = "5432";
-  database = "chainsolver_db_v3";
-}
-
-// // /**PROUCCIÓN  */
+// /**PROUCCIÓN  */
 // export function conexion() {
 //   const pool = new Pool({
 //     host: "10.116.0.2",
@@ -44,24 +21,14 @@ if (process.env.NODE_ENV == "production") {
 //   return pool;
 // }
 
-// /** DESARROLLO */
-// export function conexion() {
-//   const pool = new Pool({
-//     host: "67.205.129.62",
-//     user: "chainsolver",
-//     password: "Fr3sc0l1t4+",
-//     port: "5432",
-//     database: "chainsolver_db_v3",
-//   });
-//   return pool;
-// }
+/** DESARROLLO */
 export function conexion() {
   const pool = new Pool({
-    host: host,
-    user: user,
-    password: password,
-    port: port,
-    database: database,
+    host: "67.205.129.62",
+    user: "chainsolver",
+    password: "Fr3sc0l1t4+",
+    port: "5432",
+    database: "chainsolver_db_v3",
   });
   return pool;
 }
