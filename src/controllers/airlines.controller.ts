686653845ba05e283083010775661a3c248548e7
unknown <ophetel@gmail.com>
2024-01-26 11:03:59 -0500

    listado aerolineas

--- src/controllers/airlines.controller.ts
+++ src/controllers/airlines.controller.ts
@@ -2,12 +2,23 @@ import { Request, Response } from "express";
 
 import { conexion } from "../routes/databasePGOp";
 import * as pg from "pg";
+import { ModelAirlines } from "interface/airlines";
 const { Pool } = pg;
 const pool = conexion();
 
-export const getAilrines = async (req: Request, res: Response) => {
+export const ListarAirlines = async (req: Request, res: Response) => {
+  const modelAirlines: ModelAirlines = req.query;
   await pool.query(
-    "SELECT * FROM Table_Airlines_listar()",
+    "SELECT * FROM function_airlines_listar($1,$2,$3,$4,$5,$6,$7)",
+    [
+      modelAirlines.id_branch,
+      modelAirlines.code,
+      modelAirlines.code_iata,
+      modelAirlines.code_icao,
+      modelAirlines.name,
+      modelAirlines.status,
+      modelAirlines.id_pais,
+    ],
     (err, response, fields) => {
       if (!err) {
         let rows = response.rows;
@@ -30,3 +41,29 @@ export const getAilrines = async (req: Request, res: Response) => {
     }
   );
 };
+
+// export const getAilrines = async (req: Request, res: Response) => {
+//   await pool.query(
+//     "SELECT * FROM Table_Airlines_listar()",
+//     (err, response, fields) => {
+//       if (!err) {
+//         let rows = response.rows;
+//         if (!!rows[0].estadoflag) {
+//           res.json({
+//             status: 200,
+//             statusBol: true,
+//             data: rows,
+//           });
+//         } else {
+//           res.json({
+//             status: 200,
+//             statusBol: true,
+//             mensaje: rows[0].mensaje,
+//           });
+//         }
+//       } else {
+//         console.log(err);
+//       }
+//     }
+//   );
+// };
---
+++ src/interface/airlines.ts
@@ -0,0 +1,11 @@
+export interface ModelAirlines {
+  id?: number;
+  code_iata?: String;
+  name?: String;
+  code_icao?: String;
+  code_three?: bigint;
+  id_pais?: number;
+  code?: String;
+  status?: boolean;
+  id_branch?: number;
+}
