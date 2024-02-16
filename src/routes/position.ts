43adcb4b91ac6e80c42555dc741549950e3f2b2e
unknown <ophetel@gmail.com>
2024-02-16 16:12:43 -0500

    poition

--- src/routes/position.ts
+++ src/routes/position.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { TokenValidation } from "../libs/verifyToken";
 const router: Router = Router();
 
-import { CargarPosicion } from "../controllers/Position.controller";
+import { CargarPosicion } from "../controllers/position.controller";
 
 router.get("/cargar_posicion", TokenValidation, CargarPosicion);
 
