ca9a986ed3b20b14efaf3663146b8910519df277
unknown <ophetel@gmail.com>
2024-02-20 09:16:03 -0500

    posiciones

--- src/controllers/position.controller.ts
+++ src/controllers/position.controller.ts
@@ -28,3 +28,26 @@ export const CargarPosicion = async (req: Request, res: Response) => {
     }
   );
 };
+
+export const ListarPosicion = async (req: Request, res: Response) => {
+  const position: IPosition = req.query;
+  await pool.query(
+    "SELECT * FROM function_position_listar($1)",
+    [position.id_branch, position.code, position.name, position.description],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        res.json({
+          status: 200,
+          statusBol: true,
+          mensaje: rows[0].mensaje,
+          estadoflag: rows[0].estadoflag,
+          data: rows,
+          token: renewTokenMiddleware(req),
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
--- src/routes/position.ts
+++ src/routes/position.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 import { TokenValidation } from "../libs/verifyToken";
 const router: Router = Router();
 
-import { CargarPosicion } from "../controllers/position.controller";
+import {
+  CargarPosicion,
+  ListarPosicion,
+} from "../controllers/position.controller";
 
 router.get("/cargar_posicion", TokenValidation, CargarPosicion);
+router.get("/listar_posicion", TokenValidation, ListarPosicion);
 
 export default router;
