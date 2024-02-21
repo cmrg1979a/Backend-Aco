013fa6c40a75fd7be015f35cb4aee7501f834802
unknown <ophetel@gmail.com>
2024-02-21 09:05:35 -0500

    mantenimiento usuarios y posiciones

--- src/controllers/position.controller.ts
+++ src/controllers/position.controller.ts
@@ -32,8 +32,97 @@ export const CargarPosicion = async (req: Request, res: Response) => {
 export const ListarPosicion = async (req: Request, res: Response) => {
   const position: IPosition = req.query;
   await pool.query(
-    "SELECT * FROM function_position_listar($1)",
-    [position.id_branch, position.code, position.name, position.description],
+    "SELECT * FROM function_position_listar($1,$2,$3,$4)",
+    [
+      position.code ? position.code : null,
+      position.name ? position.name : null,
+      position.description ? position.description : null,
+      Boolean(position.estado) ? position.estado : null,
+    ],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        res.json({
+          status: 200,
+          statusBol: true,
+          mensaje: rows[0].mensaje,
+          estadoflag: rows[0].estadoflag,
+          data: rows,
+          token: renewTokenMiddleware(req),
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const verPosicion = async (req: Request, res: Response) => {
+  const position: IPosition = req.query;
+  await pool.query(
+    "SELECT * FROM function_position_ver($1,$2)",
+    [position.id, position.id_branch],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        res.json({
+          status: 200,
+          statusBol: true,
+          mensaje: rows[0].mensaje,
+          estadoflag: rows[0].estadoflag,
+          data: rows,
+          token: renewTokenMiddleware(req),
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+export const CargarUsuarioNoAsignadoPosicion = async (
+  req: Request,
+  res: Response
+) => {
+  const position: IPosition = req.query;
+  await pool.query(
+    "SELECT * FROM function_user_noasignadosposicion($1,$2)",
+    [position.id, position.id_branch],
+    (err, response, fields) => {
+      if (!err) {
+        let rows = response.rows;
+        res.json({
+          status: 200,
+          statusBol: true,
+          mensaje: rows[0].mensaje,
+          estadoflag: rows[0].estadoflag,
+          data: rows,
+          token: renewTokenMiddleware(req),
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+
+export const insertarActualizarUsuarioPosicion = async (
+  req: Request,
+  res: Response
+) => {
+  const position = req.body;
+  await pool.query(
+    "SELECT * FROM function_user_position_insertar($1,$2,$3,$4)",
+    [
+      position.user_position.map((element) => {
+        return element.id ? element.id : null;
+      }),
+      position.id,
+      position.user_position.map((element) => {
+        return element.id_users;
+      }),
+      position.user_position.map((element) => {
+        return element.estado;
+      }),
+    ],
     (err, response, fields) => {
       if (!err) {
         let rows = response.rows;
--- src/routes/position.ts
+++ src/routes/position.ts
@@ -4,10 +4,24 @@ const router: Router = Router();
 
 import {
   CargarPosicion,
+  CargarUsuarioNoAsignadoPosicion,
   ListarPosicion,
+  insertarActualizarUsuarioPosicion,
+  verPosicion,
 } from "../controllers/position.controller";
 
 router.get("/cargar_posicion", TokenValidation, CargarPosicion);
 router.get("/listar_posicion", TokenValidation, ListarPosicion);
+router.get("/ver_posicion", TokenValidation, verPosicion);
+router.get(
+  "/cargar_usuario_no_asignado_posicion",
+  TokenValidation,
+  CargarUsuarioNoAsignadoPosicion
+);
+router.put(
+  "/insertar_actualizar_posicion",
+  TokenValidation,
+  insertarActualizarUsuarioPosicion
+);
 
 export default router;
