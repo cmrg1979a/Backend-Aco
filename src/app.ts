133061c76812587913cdfb6f9166f8c85638464e
edison rosales <edisonrosalescampos@gmail.com>
2024-03-07 13:44:50 -0400

    correccion import controller

--- src/app.ts
+++ src/app.ts
@@ -116,7 +116,8 @@ import transport from "./routes/transport";
 import gasto from "./routes/gasto";
 import ingreso from "./routes/ingreso";
 import tipocostos from "./routes/tipocosto";
-import comentariosRoute from "./routes/comentarios";
+import comentariosRoute from "./routes/comentarios"
+
 
 // settings
 app.set("port", 9200);
--- src/routes/comentarios.ts
+++ src/routes/comentarios.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router: Router = Router();
 import { TokenValidation } from "../libs/verifyToken";
 
-import { getComentariosPredefinidos } from "controllers/comentarios.controller";
+import { getComentariosPredefinidos } from "../controllers/comentarios.controller";
 
 router.get("/getComentariosPredefinidos", TokenValidation, getComentariosPredefinidos);
 
