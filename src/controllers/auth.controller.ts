83f494b8ca176db7cca589a8ad12298ebd7122ac
unknown <ophetel@gmail.com>
2024-03-11 23:05:20 -0500

    cambio

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -26,7 +26,7 @@ export const singin = async (req: Request, res: Response) => {
             { data },
             process.env.TOKEN_SECRET || "tokentest",
             {
-              expiresIn: 60 * 60 * 2,
+              expiresIn: 60 * 60 * 8,
             }
           );
           res.json({
