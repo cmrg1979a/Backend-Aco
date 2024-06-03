da1512841bd6ab57c78a327023aa9ba7ebd3ae84
unknown <ophetel@gmail.com>
2024-06-03 18:13:38 -0500

    cambios

--- src/app.ts
+++ src/app.ts
@@ -17,25 +17,26 @@ const corsOptions = {
 const corsMiddleware = cors(corsOptions);
 
 /**PROUCCIÓN */
-// const pool = new Pool({
-//   host: "10.116.0.2",
-//   // host: "157.230.14.98",//// remoto
-//   user: "postgres",
-//   password: "@Developer2021Pic",
-//   port: "5432",
-//   database: "db_op_main_01",
-// });
+const pool = new Pool({
+  host: "10.116.0.2",
+  // host: "157.230.14.98",//// remoto
+  user: "postgres",
+  password: "@Developer2021Pic",
+  port: "5432",
+  database: "db_op_main_01",
+});
+
 /** DESARROLLO */
 // Cache de opciones de cors
-const pool = new Pool({
-  host: "67.205.129.62",
-  user: "chainsolver",
-  password: "Fr3sc0l1t4+",
-  port: "5432",
-  // database: "db_op_main_dev",
-  // database: "db_op_main_02",
-  database: "db_op_main_qa",
-});
+// const pool = new Pool({
+//   host: "67.205.129.62",
+//   user: "chainsolver",
+//   password: "Fr3sc0l1t4+",
+//   port: "5432",
+//   // database: "db_op_main_dev",
+//   // database: "db_op_main_02",
+//   database: "db_op_main_qa",
+// });
 
 const app: Application = express();
 
