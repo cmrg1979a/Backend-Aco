31cd02f31c047d36433533ce965419c51ff4ef0d
Pierre Zurita <cjpierrezr@gmail.com>
2025-07-19 03:00:20 -0500

    cambios

--- src/routes/databasePGOp.ts
+++ src/routes/databasePGOp.ts
@@ -6,21 +6,24 @@ console.log("esProduccion", global.esProduccion);
 function conexion() {
   const config = global.esProduccion
     ? {
-        host: "10.116.0.2",
+        host: "10.116.0.15",
         user: "postgres",
         password: "@Developer2021Pic",
         port: "5432",
         database: "db_op_main_01",
       }
     : {
-        host: "67.205.129.62",
-        user: "chainsolver",
-        password: "Fr3sc0l1t4+",
+        // host: "67.205.129.62",
+        // host: "143.244.169.120",
+        host: "10.116.0.15",
+        user: "postgres",
+        password: "@Developer2021Pic",
         port: "5432",
-        database: "db_op_main_qa",
+        database: "db_op_main_01",
       };
 
   return new Pool(config);
 }
+
 
 export { conexion };
